44a73769032e9b78e2f2d7ea8b906eb5ff80a297
CarlosYaelCervantesRojas <carlos.yael.cervantes.rojas@gmail.com>
2024-11-14 19:44:42 -0600

    main branch fixed

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,15 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  //setLocalStorage("so-cart", product);
-  const currentCart = getLocalStorage("so-cart") || [];
-  currentCart.push(product);
-  setLocalStorage("so-cart", currentCart);
+  setLocalStorage("so-cart", product);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
-  //console.log(e.target.dataset.id)
+  
   addProductToCart(product);
 }
 
